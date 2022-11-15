e2d13d760a6e971cbb2266c780ea6029f9bd727f
Jarom Burr <jaromburr@hotmail.com>
2022-11-15 08:50:26 -0700

    this is the working version :)

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@
 #define HEIGHT 600
 
 int main() {
-  std::ifstream vectorField("cyl2d_1300x600_float[2].raw", std::ios::binary);
+  std::ifstream vectorField("cyl2d_1300x600_float32[2].raw", std::ios::binary);
   if (vectorField.is_open()) {
     // Get the length of the image should be 3145728
+    std::cout << "opened" << std::endl;
     vectorField.seekg(0, std::ios_base::end);
     auto length = vectorField.tellg();
     vectorField.seekg(0, std::ios::beg);
@@ -32,9 +33,11 @@ int main() {
     }
 
     // Writing output to file
-    std::fstream outField("outfield.raw", std::ios::binary);
+    std::fstream outField("outfield.raw", std::ios::out | std::ios::binary);
     outField.write(reinterpret_cast<char *>(output), length);
     outField.close();
+  } else {
+    std::cout << "Didn't open" << std::endl;
   }
   return 0;
 }
