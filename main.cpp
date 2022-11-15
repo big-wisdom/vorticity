792cfe70e75d9dadc333f237685493875aa62df3
Jarom Burr <jaromburr@hotmail.com>
2022-11-15 07:54:01 -0700

    file_name

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <vorticity.cpp>
 
 int main() {
-  std::ifstream vectorField("gc_1024x1024.raw", std::ios::binary);
+  std::ifstream vectorField("cyl2d_1300x600_float[2].raw", std::ios::binary);
   if (vectorField.is_open()) {
     // Get the length of the image should be 3145728
     vectorField.seekg(0, std::ios_base::end);
