91278112e485073e9cacc2197c47e0941823e9b3
Jarom Burr <jaromburr@hotmail.com>
2022-11-15 07:47:03 -0700

    sorry me code bad

--- main.cpp
+++ main.cpp
@@ -4,26 +4,24 @@
 #include <math.h>
 #include <vector>
 
-
 int main() {
   std::ifstream image("gc_1024x1024.raw", std::ios::binary);
   if (image.is_open()) {
-    //Get the length of the image should be 3145728
+    // Get the length of the image should be 3145728
     image.seekg(0, std::ios_base::end);
     auto length = image.tellg();
-    image.seekg (0, std::ios::beg);
-    
-    //Initialize arrays
+    image.seekg(0, std::ios::beg);
+
+    // Initialize arrays
     unsigned char *input = new unsigned char[length];
     unsigned char *outputGPUGlobal = new unsigned char[length];
     unsigned char *outputGPUShared = new unsigned char[length];
     unsigned char *outputCPU = new unsigned char[length];
-    
-    //Get rgb values from image into input array 
-    image.read((char*)input, length);
+
+    // Get rgb values from image into input array
+    image.read((char *)input, length);
     image.close();
 
-  
-  return 0;
+    return 0;
+  }
 }
-
--- vorticity.cpp
+++ vorticity.cpp
@@ -1,30 +1,33 @@
 
+#include <cstdint>
+
+#include <utility>
 typedef std::pair<double, double> Vec2d;
 
 /*
-	Compute vorticity feature at cell coordinate (x, y) of the field f
+        Compute vorticity feature at cell coordinate (x, y) of the field f
 */
-float vorticity(int x, int y, int width, int height, float* f){
-    float dx=0.01;
-    float dy=0.01;
+float vorticity(int x, int y, int width, int height, float *f) {
+  float dx = 0.01;
+  float dy = 0.01;
 
-    uint32_t idx = y*width+x; 
-    
-    int start_x = (x == 0) ? 0 : x - 1;
-    int end_x = (x == width-1) ? x : x + 1;
+  uint32_t idx = y * width + x;
 
-    int start_y = (y == 0) ? 0 : y - 1;
-    int end_y = (y == height-1) ? y : y + 1;
+  int start_x = (x == 0) ? 0 : x - 1;
+  int end_x = (x == width - 1) ? x : x + 1;
 
-    uint32_t duidx = (start_y*width + end_x)*2;
-    uint32_t dvidx = (end_y*width + start_x)*2;
+  int start_y = (y == 0) ? 0 : y - 1;
+  int end_y = (y == height - 1) ? y : y + 1;
 
-    Vec2d fdu(f[duidx],f[duidx+1]);
-    Vec2d fdv(f[dvidx],f[dvidx+1]);
-    Vec2d vec0(f[idx*2],f[idx*2+1]);
+  uint32_t duidx = (start_y * width + end_x) * 2;
+  uint32_t dvidx = (end_y * width + start_x) * 2;
 
-    float duy = (fdu.second - vec0.second)/(dx*(end_x-start_x));
-    float dvx = (fdv.first - vec0.first)/(dy*(end_y-start_y));
+  Vec2d fdu(f[duidx], f[duidx + 1]);
+  Vec2d fdv(f[dvidx], f[dvidx + 1]);
+  Vec2d vec0(f[idx * 2], f[idx * 2 + 1]);
 
-    return duy-dvx;
+  float duy = (fdu.second - vec0.second) / (dx * (end_x - start_x));
+  float dvx = (fdv.first - vec0.first) / (dy * (end_y - start_y));
+
+  return duy - dvx;
 }
