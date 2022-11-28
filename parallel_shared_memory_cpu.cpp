197bf1487c3b1ef2e523da2a642c7796a0f32abf
Eli Hermann <Eli_Hermann@hotmail.com>
2022-11-28 15:34:06 -0700

    parallel shared memory CPU complete.

--- parallel_shared_memory_cpu.cpp
+++ parallel_shared_memory_cpu.cpp
@@ -7,10 +7,12 @@
 int thread_count = 4;
 
 void parallel_shared_memory_cpu(int HEIGHT, int WIDTH, float * input, unsigned char * output) {
-// #pragma omp parallel num_threads(thread_count) default(none) shared(temp, a, n) private(i, j, count)
+    int i, j;
+    #pragma omp parallel num_threads(thread_count) default(none) shared(output, input, HEIGHT, WIDTH) private(i, j)
     {
-        for (int i = 0; i < HEIGHT; i++) {
-          for (int j = 0; j < WIDTH; j++) {
+        #pragma omp for
+        for (i = 0; i < HEIGHT; i++) {
+          for (j = 0; j < WIDTH; j++) {
             float vort = vorticity(j, i, WIDTH, HEIGHT, input);
             unsigned char vortChar;
             if (vort < -0.2f) {
