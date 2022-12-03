fde9420282973031c247d4edfdf4c11e1f15cc22
Jarom Burr <jaromburr@hotmail.com>
2022-12-03 12:47:25 -0700

    Instructions and changed name of file to verfiy

--- parallel_shared_memory_gpu.cu
+++ parallel_shared_memory_gpu.cu
@@ -1,3 +1,13 @@
+/*
+  This code is meant to be run on a cuda device. 
+  You must first use the command module load cuda 
+  to ensure that you can compile correctly with 
+  nvcc parallel_shared_memory_gpu.cu -o object_name.
+  Once the code is compiled you can run then run the
+  code with ./object_name assuming you have prepared 
+  the gpu correctly. 
+*/
+
 #include <cstring> 
 #include <fstream>
 #include <iostream>
@@ -13,8 +23,6 @@
 #define BLOCK_HEIGHT 30
 #define HALO 2
 #define CHANNELS 2
-
-//This doesn't work just the beginnings of an idea
 
 __global__
 void convertTile(int height, int width, unsigned char *output, float *input) {
@@ -57,39 +65,16 @@ void convertTile(int height, int width, unsigned char *output, float *input) {
   vortTile[threadIdx.x + 1][threadIdx.y + 1][1] = input[(CHANNELS * ((y * WIDTH) + x)) + 1];
   __syncthreads();
 
-  if (threadIdx.x == 0 && threadIdx.y == 0)
-  {
-    for(int i = 0; i < 32; i ++){
-      for (int j = 0; j < 22; j++) {
-        int zeroX = 0 + (blockIdx.x * blockDim.x);
-        int zeroY = 0 + (blockIdx.y * blockDim.y);
-        int newX = zeroX + j - 1;
-        int newY = zeroY + i - 1;
-        if( (i != 0 || j != 0) && (j != 21 || i != 0) && (j != 0 || i != 31) && (j != 21 || i != 31)) {
-          if (newX > -1 && newX < width && newY > -1 && newY < height ) {
-            if (vortTile[j][i][0] != input[2 * ((newY * width) + newX)] || vortTile[j][i][1] != input[2 * ((newY * width) + newX) + 1]) {
-              printf("x: %d y: %d Tile x: %f Tile y: %f input x: %f input y: %f\n",newX, newY,vortTile[j][i][0], vortTile[j][i][1], input[2 * ((newY * width) + newX)], input[2 * ((newY * width) + newX) + 1]);
-            }
-          }
-        }
-      }
-    }
-  }
-
+  //I am not sure if cuda can call a function that is in another file so I just put this here. 
   //The vorticity funciton
   float dx = 0.01;
   float dy = 0.01;
-
-  uint32_t idx = y * width + x;
 
   int start_x = (x == 0) ? 1 : threadIdx.x;
   int end_x = (x == width - 1) ? threadIdx.x + 1: threadIdx.x + 2;
 
   int start_y = (y == 0) ? 1 : threadIdx.y;
   int end_y = (y == height - 1) ? threadIdx.y + 1: threadIdx.y + 2;
-
-  uint32_t duidx = (start_y * width + end_x) * 2;
-  uint32_t dvidx = (end_y * width + start_x) * 2;
 
   double fdu[2] = {vortTile[end_x][start_y][0], vortTile[end_x][start_y][1]};
   double fdv[2] = {vortTile[start_x][end_y][0], vortTile[start_x][end_y][1]};
@@ -99,38 +84,6 @@ void convertTile(int height, int width, unsigned char *output, float *input) {
 
   float vort = duy - dvx;
   //End of vorticity function
-
-  // add old vorticity function and run on input compare outputs and see if vorticity is messing something up 
-  //The vorticity funciton
-  dx = 0.01;
-  dy = 0.01;
-
-  idx = y * width + x;
-
-  start_x = (x == 0) ? 0 : x - 1;
-  end_x = (x == width - 1) ? x : x + 1;
-
-  start_y = (y == 0) ? 0 : y - 1;
-  end_y = (y == height - 1) ? y : y + 1;
-
-  duidx = (start_y * width + end_x) * 2;
-  dvidx = (end_y * width + start_x) * 2;
-
-  double fdu2[2] = {input[duidx], input[duidx + 1]};
-  double fdv2[2] = {input[dvidx], input[dvidx + 1]};
-  double vec02[2] = {input[idx * 2], input[idx * 2 + 1]};
-
-  float duy2 = (fdu2[1] - vec02[1]) / (dx * (end_x - start_x));
-  float dvx2 = (fdv2[0] - vec02[0]) / (dy * (end_y - start_y));
-
-  float vort2 = duy2 - dvx2;
-  //End of vorticity function 
-  if (dvx != dvx2 && blockIdx.x == 1 and blockIdx.y == 0) {
-    printf("problem tile:%f original: %f at x: %d y: %d\n", dvx, dvx2, x, y);
-  }
-  if (fdv[0] != fdv2[0] && blockIdx.x == 1 and blockIdx.y == 0) {
-    printf("fdv problem tile:%f original: %f at x: %d y: %d\n", fdv[0], fdv2[0], x, y);
-  }
 
   unsigned char vortChar;
     if (vort < -0.2f) {
@@ -145,6 +98,7 @@ void convertTile(int height, int width, unsigned char *output, float *input) {
 
 }
 
+//An old global convert function
 __global__
 void convert(int height, int width, unsigned char *output, float *input) {
   int x = threadIdx.x + blockIdx.x * blockDim.x;
@@ -232,7 +186,7 @@ int main() {
     //parallel_shared_memory_cpu(HEIGHT, WIDTH, input, output);
     parallel_shared_memory_gpu(HEIGHT, WIDTH, input, output, length);
     // Writing output to file
-    std::fstream outField("outfield.raw", std::ios::out | std::ios::binary);
+    std::fstream outField("outfieldGpu.raw", std::ios::out | std::ios::binary);
     outField.write(reinterpret_cast<char *>(output), length / CHANNELS);
     outField.close();
   } else {
