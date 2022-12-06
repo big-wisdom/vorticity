ecf77e4e79efe8467416a9ddd36a8b18087a8137
Jarom Burr <jaromburr@hotmail.com>
2022-12-05 22:45:54 -0700

    shared memroy gpu dynamiv

--- vorticity_1-3/psm_gpu_original.cu
+++ vorticity_1-3/psm_gpu_original.cu
@@ -37,42 +37,43 @@
 __global__
 void convertTile(int height, int width, unsigned char *output, float *input, const int block_width, const int block_height) {
 
-  __shared__ float vortTile[blockDim.x + 2][blockDim.y + 2][2];
+  extern __shared__ float shared[];
+  float* vortTile = shared;
 
   int x = threadIdx.x + (blockIdx.x * blockDim.x);
   int y = threadIdx.y + (blockIdx.y * blockDim.y);
 
   // Copy over the vector information to the tile
-  
+  printf("Here 1\n");
   if (threadIdx.x == 0 && x != 0) { //get Left Halo
-    vortTile[threadIdx.x][threadIdx.y + 1][0] = input[CHANNELS * ((y * width) + (x - 1))];
-    vortTile[threadIdx.x][threadIdx.y + 1][1] = input[(CHANNELS * ((y * width) + (x - 1))) + 1];
+    vortTile[CHANNELS * ((threadIdx.y + 1) * block_width + threadIdx.x)] = input[CHANNELS * ((y * width) + (x - 1))];
+    vortTile[CHANNELS * ((threadIdx.y + 1) * block_width + threadIdx.x) + 1] = input[(CHANNELS * ((y * width) + (x - 1))) + 1];
   }
-  
+  printf("Here 2");
   if (threadIdx.x == (block_width - 1) && x != width - 1) { //get Right Halo
-    vortTile[threadIdx.x + 2][threadIdx.y + 1][0] = input[CHANNELS * ((y * width) + (x + 1))];
-    vortTile[threadIdx.x + 2][threadIdx.y + 1][1] = input[(CHANNELS * ((y * width) + (x + 1))) + 1];
+    vortTile[CHANNELS * ((threadIdx.y + 1) * block_width + (threadIdx.x + 2))] = input[CHANNELS * ((y * width) + (x + 1))];
+    vortTile[CHANNELS * ((threadIdx.y + 1) * block_width + (threadIdx.x + 2)) + 1] = input[(CHANNELS * ((y * width) + (x + 1))) + 1];
   }
- 
+  printf("Here 3");
   if (threadIdx.y == 0 && y != 0) { //get Upper Halo
     if (threadIdx.x == (block_width - 1) && x != width - 1) { //get Upper Right Corner 
-      vortTile[threadIdx.x + 2][threadIdx.y][0] = input[CHANNELS * (((y - 1) * width) + (x + 1))];
-      vortTile[threadIdx.x + 2][threadIdx.y][1] = input[CHANNELS * (((y - 1) * width) + (x + 1)) + 1];
+      vortTile[CHANNELS * ((threadIdx.y) * block_width + (threadIdx.x + 2))] = input[CHANNELS * (((y - 1) * width) + (x + 1))];
+      vortTile[CHANNELS * ((threadIdx.y) * block_width + (threadIdx.x + 2))] = input[CHANNELS * (((y - 1) * width) + (x + 1)) + 1];
     }
-    vortTile[threadIdx.x + 1][threadIdx.y][0] = input[CHANNELS * (((y - 1) * width) + x)];
-    vortTile[threadIdx.x + 1][threadIdx.y][1] = input[(CHANNELS * (((y - 1) * width) + x)) + 1];
+    vortTile[CHANNELS * ((threadIdx.y) * block_width + (threadIdx.x + 1))] = input[CHANNELS * (((y - 1) * width) + x)];
+    vortTile[CHANNELS * ((threadIdx.y) * block_width + (threadIdx.x + 1)) + 1] = input[(CHANNELS * (((y - 1) * width) + x)) + 1];
   }
-  
+  printf("Here 4");
   if (threadIdx.y == (block_height - 1) && y != height - 1) { // Get Lower Halo
     if (threadIdx.x == 0 && x != 0) { //get Lower Left Corner
-      vortTile[threadIdx.x][threadIdx.y + 2][0] = input[CHANNELS * (((y + 1) * width) + (x - 1))];  
-      vortTile[threadIdx.x][threadIdx.y + 2][1] = input[(CHANNELS * (((y + 1) * width) + (x - 1))) + 1];
+      vortTile[CHANNELS * ((threadIdx.y + 2) * block_width + (threadIdx.x))] = input[CHANNELS * (((y + 1) * width) + (x - 1))];  
+      vortTile[CHANNELS * ((threadIdx.y + 2) * block_width + (threadIdx.x)) + 1] = input[(CHANNELS * (((y + 1) * width) + (x - 1))) + 1];
     }
-    vortTile[threadIdx.x + 1][threadIdx.y + 2][0] = input[CHANNELS * (((y + 1) * width) + x)];  
-    vortTile[threadIdx.x + 1][threadIdx.y + 2][1] = input[(CHANNELS * (((y + 1) * width) + x)) + 1];
+    vortTile[CHANNELS * ((threadIdx.y + 2) * block_width + (threadIdx.x + 1))] = input[CHANNELS * (((y + 1) * width) + x)];  
+    vortTile[CHANNELS * ((threadIdx.y + 2) * block_width + (threadIdx.x + 1)) + 1] = input[(CHANNELS * (((y + 1) * width) + x)) + 1];
   }
-  vortTile[threadIdx.x + 1][threadIdx.y + 1][0] = input[CHANNELS * ((y * width) + x)];
-  vortTile[threadIdx.x + 1][threadIdx.y + 1][1] = input[(CHANNELS * ((y * width) + x)) + 1];
+  vortTile[CHANNELS * ((threadIdx.y + 1) * block_width + (threadIdx.x + 1))] = input[CHANNELS * ((y * width) + x)];
+  vortTile[CHANNELS * ((threadIdx.y + 1) * block_width + (threadIdx.x + 1)) + 1] = input[(CHANNELS * ((y * width) + x)) + 1];
   __syncthreads();
 
   //I am not sure if cuda can call a function that is in another file so I just put this here. 
@@ -80,15 +81,20 @@ void convertTile(int height, int width, unsigned char *output, float *input, con
   float dx = 0.01;
   float dy = 0.01;
 
+  uint32_t idx = (threadIdx.y + 1) * block_width + threadIdx.x + 1;
+
   int start_x = (x == 0) ? 1 : threadIdx.x;
   int end_x = (x == width - 1) ? threadIdx.x + 1: threadIdx.x + 2;
 
   int start_y = (y == 0) ? 1 : threadIdx.y;
   int end_y = (y == height - 1) ? threadIdx.y + 1: threadIdx.y + 2;
 
-  double fdu[2] = {vortTile[end_x][start_y][0], vortTile[end_x][start_y][1]};
-  double fdv[2] = {vortTile[start_x][end_y][0], vortTile[start_x][end_y][1]};
-  double vec0[2] = {vortTile[threadIdx.x + 1][threadIdx.y + 1][0], vortTile[threadIdx.x + 1][threadIdx.y + 1][1]};
+  uint32_t duidx = (start_y * block_width + end_x) * 2;
+  uint32_t dvidx = (end_y * block_width + start_x) * 2;
+
+  double fdu[2] = {vortTile[duidx], vortTile[duidx + 1]};
+  double fdv[2] = {vortTile[dvidx], vortTile[dvidx + 1]};
+  double vec0[2] = {vortTile[idx], vortTile[idx + 1]};
   float duy = (fdu[1] - vec0[1]) / (dx * (end_x - start_x));
   float dvx = (fdv[0] - vec0[0]) / (dy * (end_y - start_y));
 
@@ -150,7 +156,7 @@ void convert(int height, int width, unsigned char *output, float *input) {
   output[y * width + x] = vortChar;
 }
 
-extern "C" void parallel_shared_memory_gpu(int height, int width, float * input, unsigned char * output, int length) {
+extern "C" float parallel_shared_memory_gpu(int height, int width, float * input, unsigned char * output, int length, int block_width, int block_height, int grid_width, int grid_height) {
     //Prepare cuda stuff
     float *inputDevice;
     unsigned char * outputDevice;
@@ -168,9 +174,10 @@ extern "C" void parallel_shared_memory_gpu(int height, int width, float * input,
     //get Tile and Block size 
     const dim3 block_size (block_width, block_height);
     const dim3 grid_size (grid_width, grid_height);
+    unsigned shared_mem_size = block_width * block_height * sizeof(float);
 
     cudaEventRecord(startEvent, 0);
-    convertTile<<<grid_size, block_size>>>(height, width, outputDevice, inputDevice, block_width, block_height);
+    convertTile<<<grid_size, block_size, shared_mem_size>>>(height, width, outputDevice, inputDevice, block_width, block_height);
     cudaEventRecord(stopEvent, 0);
     cudaEventSynchronize(stopEvent);
     cudaEventElapsedTime(&sharedTime, startEvent, stopEvent);
@@ -186,34 +193,35 @@ extern "C" void parallel_shared_memory_gpu(int height, int width, float * input,
 }
 
 
-// int main() {
-//   std::ifstream vectorField("cyl2d_1300x600_float32[2].raw", std::ios::binary);
-//   if (vectorField.is_open()) {
-//     // Get the length of the image should be 3145728
-//     std::cout << "opened" << std::endl;
-//     vectorField.seekg(0, std::ios_base::end);
-//     auto length = vectorField.tellg();
-//     vectorField.seekg(0, std::ios::beg);
-// 
-//     auto fl_size = sizeof(float);
-// 
-//     // Initialize arrays
-//     float *input = new float[length / fl_size];
-//     unsigned char *output = new unsigned char[length / fl_size / CHANNELS];
-// 
-//     // Get rgb values from image into input array
-//     vectorField.read((char *)input, length);
-//     vectorField.close();
-// 
-//     // serial_vorticity(HEIGHT, WIDTH, input, output);
-//     //parallel_shared_memory_cpu(HEIGHT, WIDTH, input, output);
-//     parallel_shared_memory_gpu(HEIGHT, WIDTH, input, output, length);
-//     // Writing output to file
-//     std::fstream outField("outfieldGpu.raw", std::ios::out | std::ios::binary);
-//     outField.write(reinterpret_cast<char *>(output), length / CHANNELS);
-//     outField.close();
-//   } else {
-//     std::cout << "Didn't open" << std::endl;
-//   }
-//   return 0;
-// }
+int main() {
+  std::ifstream vectorField("cyl2d_1300x600_float32[2].raw", std::ios::binary);
+  if (vectorField.is_open()) {
+    // Get the length of the image should be 3145728
+    std::cout << "opened" << std::endl;
+    vectorField.seekg(0, std::ios_base::end);
+    auto length = vectorField.tellg();
+    vectorField.seekg(0, std::ios::beg);
+
+    auto fl_size = sizeof(float);
+
+    // Initialize arrays
+    float *input = new float[length / fl_size];
+    unsigned char *output = new unsigned char[length / fl_size / CHANNELS];
+
+    // Get rgb values from image into input array
+    vectorField.read((char *)input, length);
+    vectorField.close();
+
+    // serial_vorticity(HEIGHT, WIDTH, input, output);
+    //parallel_shared_memory_cpu(HEIGHT, WIDTH, input, output);
+    float time = parallel_shared_memory_gpu(HEIGHT, WIDTH, input, output, length, 20, 30, 65, 20);
+    std::cout << "Time: " << time << std::endl;
+    // Writing output to file
+    std::fstream outField("outfieldGpu.raw", std::ios::out | std::ios::binary);
+    outField.write(reinterpret_cast<char *>(output), length / CHANNELS);
+    outField.close();
+  } else {
+    std::cout << "Didn't open" << std::endl;
+  }
+  return 0;
+}
